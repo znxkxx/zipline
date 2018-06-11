2c869a4f84ae8ea5ed3a4dec44dbc96c7466cbb9
Jacob Nazarenko <jnazarenko@quantopian.com>
2018-06-11 18:06:59 -0400

    ENH: Add command line argument support for custom types

--- zipline/algorithm/__init__.py
+++ zipline/algorithm/__init__.py
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from algorithm import TradingAlgorithm
-from simulated_algorithm import SimulatedTradingAlgorithm
+from .simulated_algorithm import SimulatedTradingAlgorithm
+from .algorithm import TradingAlgorithm
 
 __all__ = [
     'TradingAlgorithm',
